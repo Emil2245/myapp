68460ba4bf745a628e80c595c7e405117e307b46
emilmacenamiento <emilmacenamiento@gmail.com>
2024-11-25 15:37:30 +0000

    feat: import from previous repository, since it had compatibility errors

---
+++ lib/view/settings_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/view/widgets/set_sleep_time_button.dart';
+
+import '../app_icons.dart';
+
+class SettingsView extends StatefulWidget {
+  const SettingsView({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  String _storedValue = "No value saved";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(AppIcons.back),
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(
+          widget.title,
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+              color: Theme.of(context).colorScheme.onPrimary,
+              fontWeight: FontWeight.normal),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: SetSleepTimeButton(onPressed: (){})
+      ),
+    );
+  }
+
+  // Future<void> _saveValue() async {
+  //   final prefs = await SharedPreferences.getInstance();
+  //   await prefs.setString('myKey', 'Hello, Flutter!');
+  //   setState(() {
+  //     _storedValue = 'Value saved: Hello, Flutter!';
+  //   });
+  // }
+}
